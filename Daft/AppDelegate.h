99cbb030469746184adda5963bd5790360d57553
Alan Pearson Mathews <alanpearsonmathews@gmail.com>
2013-12-11 18:07:32 +0000

    Initial Commit

---
+++ Daft/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Daft
+//
+//  Created by Alan on 11/12/2013.
+//  Copyright (c) 2013 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Daft/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Daft
+//
+//  Created by Alan on 11/12/2013.
+//  Copyright (c) 2013 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
